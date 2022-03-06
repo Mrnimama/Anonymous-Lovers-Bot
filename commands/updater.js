97bc8572df7568b5f6222cbc279b798632961c34
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2022-03-06 14:01:22 +0530

    ⚕️🎭⚜️ Anonymous Bot ⚜️🎭⚕️

--- commands/updater.js
+++ commands/updater.js
@@ -14,7 +14,7 @@ const Heroku = require('heroku-client');
 const { PassThrough } = require('stream');
 const heroku = new Heroku({ token: Config.HEROKU.API_KEY })
 
-const Language = require('../language');
+const Language = require('../media');
 const Lang = Language.getString('updater');
 
 
